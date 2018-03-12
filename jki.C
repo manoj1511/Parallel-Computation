0b485ac5948bc4c5028fd93e082ab45fdde738fd
manoj1511 <34974946+manoj1511@users.noreply.github.com>
2018-03-12 13:57:05 -0500

    Add files via upload

---
+++ jki.C
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+#define N 2000
+
+int main(int argc, const char** argv)
+{
+  clock_t start_time,end_time;
+  start_time = clock();
+  int n = N;
+  static int a[N][N] ={0};
+  static int b[N][N] ={0};
+  static int c[N][N] ={0};
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            a[i][j] = rand()%100;
+            b[i][j] = rand()%100;
+        }
+    }
+           for(int j=0;j<n;j++)
+             
+              {
+                   for(int k=0;k<n;k++)
+                  {
+                       for(int i=0;i<n;i++)
+                     
+                      {
+                          c[i][j] += a[i][k] * b[k][j];
+                      }
+                    
+                  }
+                 
+              }
+       
+      
+        cout << "\nthe array size is :"<<N<<"\n";
+        cout << "the final array has been created\n";
+        cout << "time to see the performance \n";
+        end_time = clock();
+        cout<<"Time Taken is : "<<(end_time - start_time)/1000<<" ms"<<endl;
+       /*cout << "\nthe final array has been created\n\n";
+        cout << "time to see the results \n";
+       for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout<<"\t"<<c[i][j];
+            }
+            cout<<"\n\n";
+        }*/
+          
+  return 0;
+}
